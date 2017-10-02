4478f16924ea1fb89bd7a3169ea6f7a5dac6e4e1
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-10-02 09:58:34 -0700

    fix(planck): Adjust mouse movement

--- planck/config.h
+++ planck/config.h
@@ -6,8 +6,8 @@
 #define PREVENT_STUCK_MODIFIERS
 #define MOUSEKEY_DELAY             200
 #define MOUSEKEY_INTERVAL          15
-#define MOUSEKEY_MAX_SPEED         10
-#define MOUSEKEY_TIME_TO_MAX       40
+#define MOUSEKEY_MAX_SPEED         9
+#define MOUSEKEY_TIME_TO_MAX       70
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
