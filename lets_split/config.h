e3bf05cd3f16d0f654045dfafd2fd002a4a59d8d
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2018-01-07 11:56:57 -0800

    feat(keyboards): Add let_split initial planck copy

---
+++ lets_split/config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../qmk_firmware/keyboards/planck/config.h"
+
+#define PREVENT_STUCK_MODIFIERS
+#define MOUSEKEY_DELAY             200
+#define MOUSEKEY_INTERVAL          15
+#define MOUSEKEY_MAX_SPEED         9
+#define MOUSEKEY_TIME_TO_MAX       70
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 253
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PLANCK_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+#endif
+
+#endif
