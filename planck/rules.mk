b539ec3e752dcb81f1f24cf526fbd0ca8e3e2623
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2018-01-07 16:17:30 -0800

    fix(planck): Fix for new version of qmk
    See https://github.com/qmk/qmk_firmware/issues/2087

--- planck/rules.mk
+++ planck/rules.mk
@@ -5,6 +5,7 @@ RGBLIGHT_ENABLE = no  # Clashes with audio
 BLUETOOTH_ENABLE = no # No bluetooth
 SLEEP_LED_ENABLE = no # Uses BACKLIGHT_ENABLE rimer
 MOUSEKEY_ENABLE = yes # Use keyboard as mouse
+MIDI_ENABLE = no
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
