19ccd0fa081a8f2a98f8ea8971c39177e4b580eb
Berkeley True <berkeleytrue@users.noreply.github.com>
2018-02-20 13:01:24 -0800

    feat(bash/zsh): Break out common funcs for zsh/bash

---
+++ dotfiles/shell/common_paths.sh
@@ -0,0 +1,16 @@
+# Make neovim the default editor
+export EDITOR=nvim
+export VISUAL=$EDITOR
+# add gems to path
+export PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin"
+
+# Add lang settings for fonts to work properly
+export LANG=en_US.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export SSH_KEY_PATH="~/.ssh/rsa_id"
