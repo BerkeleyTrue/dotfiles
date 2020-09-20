4a1db7a2b02fcd4a89f84f59049351ed9e4256a9
Berkeley True <berkeleytrue@users.noreply.github.com>
2020-09-20 09:14:15 -0700

    fix(conf/shell): don't use conditional checks
    files that should be there should error if not found

--- .config/shell/index.sh
+++ .config/shell/index.sh
@@ -1,7 +1,5 @@
-#! /bin/bash
-
-isosx="$(uname | grep -q Darwin)"
-config="$HOME/.config/shell"
+ISOSX="$(uname | grep -q Darwin)"
+SHELL_CONF="$HOME/.config/shell"
 TERMINAL="alacritty"
 
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
@@ -9,14 +7,18 @@ export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_DATA_DIRS=${XDG_DATA_DIRS:-/usr/local/share:/usr/share:$XDG_DATA_HOME}
 
 [[ -s "$HOME/.private_aliases"  ]] && source "$HOME/.private_aliases"
-[[ -s "$config/common_paths.sh"  ]] && source "$config/common_paths.sh"
-[[ -s "$config/common_aliases.sh"  ]] && source "$config/common_aliases.sh"
-[[ -s "$config/common_helpers.sh"  ]] && source "$config/common_helpers.sh"
-[[ -s "$config/npm-helpers.sh"  ]] && source "$config/npm-helpers.sh"
-[[ -s "$config/git.sh" ]] && source "$config/git.sh"
-[[ -s "$config/.mr.sh" ]] && source "$config/.mr.sh"
-[[ isosx ]] && [[ -s "./osx.sh" ]] && source "$config/osx.sh"
+
+# don't use conditional check for files that should always be there
+source $SHELL_CONF/common_paths.sh
+source $SHELL_CONF/common_aliases.sh
+source $SHELL_CONF/common_helpers.sh
+source $SHELL_CONF/npm.sh
+source $SHELL_CONF/git.sh
+
+[[ -s "$SHELL_CONF/.mr.sh" ]] && source $SHELL_CONF/.mr.sh
+
+[[ ISOSX ]] && [[ -s "./osx.sh" ]] && source $SHELL_CONF/osx.sh
 
 # nvm
 export NVM_DIR=${NVIM_DIR:-$XDG_CONFIG_HOME/nvm}
-source $config/nvm.sh
+source $SHELL_CONF/nvm.sh
