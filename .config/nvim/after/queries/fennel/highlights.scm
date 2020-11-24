505d456d54a46ec5ae3c51caa5154d29995ad776
Berkeley True <berkeleytrue@users.noreply.github.com>
2020-11-24 15:18:25 -0800

    feat(nvim/queries/fnl): better namespace match

--- .config/nvim/after/queries/fennel/highlights.scm
+++ .config/nvim/after/queries/fennel/highlights.scm
@@ -1,7 +1,10 @@
 ((function_call
     name: (identifier) @function.macro (#eq? @function.macro "module")
     [(identifier)
-     (field_expression (identifier)+)] @namespace))
+     (field_expression
+       (identifier) @namespace
+       "."
+       (identifier) @namespace)] @namespace))
 
 ((function_call
     name: (identifier) @function.macro (#match? @function.macro "^def\-?$")))
