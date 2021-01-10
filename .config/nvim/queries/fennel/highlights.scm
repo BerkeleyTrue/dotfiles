c8665f0e2617e205b9b57bcbfa15eece7fdaf96e
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-01-09 16:30:35 -0800

    fix(nvim/queries/fennel): lint files

--- .config/nvim/queries/fennel/highlights.scm
+++ .config/nvim/queries/fennel/highlights.scm
@@ -36,19 +36,17 @@
 ;     require-macros [namespace]}
 ;    base-export-table)
 (list
-  . (
-      ((symbol) @function.macro (#eq? @function.macro "module"))
-      (symbol) @namespace))
+  .  (symbol) @function.macro (#eq? @function.macro "module")
+     (symbol) @namespace)
 
 ; defn/defn-
 (list
-  . (
-      ((symbol) @function.macro (#match? @function.macro "^defn\-?$")) ; defn macro
-      (symbol) @function ; defined function symbol
-      (array (symbol)* @parameter))) ;  args
+ .  (symbol) @function.macro (#match? @function.macro "^defn\-?$") ; defn macro
+    (symbol) @function ; defined function symbol
+    (array (symbol)* @parameter)) ;  args
 
 ; def/def-
-(list . ((symbol) @function.macro (#match? @function.macro "^def\-?$")))
+(list . (symbol) @function.macro (#match? @function.macro "^def\-?$"))
 
 ; defonce/defonce-
-(list . (symbol) @function.macro (#match? @function.macro "^defonce\-?$"))
+(list . (symbol) @function.macro (#match? @function.macro "defonce\-?$"))
