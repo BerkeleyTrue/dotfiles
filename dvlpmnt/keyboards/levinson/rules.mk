755bd06f42a7f915c4a7e5a6f468a0c09cef4775
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-01-23 15:32:08 -0800

    fix(keebs): update confs to EEHANDS

---
+++ dvlpmnt/keyboards/levinson/rules.mk
@@ -0,0 +1,7 @@
+AUDIO_ENABLE = no
+
+RGBLIGHT_ENABLE = no  # Clashes with audio
+BLUETOOTH_ENABLE = no # No bluetooth
+SLEEP_LED_ENABLE = no # Uses BACKLIGHT_ENABLE rimer
+MOUSEKEY_ENABLE = yes # Use keyboard as mouse
+MIDI_ENABLE = no
