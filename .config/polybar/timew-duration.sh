b8d6b95012f871d773b71fb9da2c33bd2d397555
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-02-08 12:54:27 -0800

    feat(polybar/timew): show active tag

--- .config/polybar/timew-duration.sh
+++ .config/polybar/timew-duration.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-SUMMARY=$(TIMEWARRIORDB=~/.config/timewarrior /usr/bin/timew summary)
+timew_cmd() {
+  TIMEWARRIORDB=~/.config/timewarrior /usr/bin/timew $@
+}
+SUMMARY=$(timew_cmd summary)
+IS_ACTIVE=$(timew_cmd get dom.active)
+TAG=$(timew_cmd get dom.active.tag.1)
 
+OUTPUT=$(
 [[ $SUMMARY = No* ]] && echo "0 mins" ||\
   echo "$SUMMARY" \
   | awk '{print $1}' \
   | tail -n 1 \
   | awk -F: '{print ($1=="0")?$2" mins": $1" hrs " $2 " mins" }'
+)
+
+if (( $IS_ACTIVE == "1" )); then
+  OUTPUT+=": '"${TAG}"'"
+fi
+
+echo $OUTPUT
