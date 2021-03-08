713c17fad4add9f2218b2ff83c663064cebb0ab2
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-03-07 21:37:48 -0800

    fix(xmonad): add ewhm event hook for full screen app requests

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -206,12 +206,7 @@ myManageHook =
 
 ------------------------------------------------------------------------
 -- Event handling
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
-myEventHook = fadeWindowsEventHook
+myEventHook = (<>) fadeWindowsEventHook fullscreenEventHook
 
 ------------------------------------------------------------------------
 -- Status bars and logging
