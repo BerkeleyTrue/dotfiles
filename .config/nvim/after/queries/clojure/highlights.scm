dfe26e4dd76b1da7022b7d48a1a4ec1fb9e0e26f
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-06-21 14:54:06 -0700

    fix(nvim/ts/clj): add fn macro query

--- .config/nvim/after/queries/clojure/highlights.scm
+++ .config/nvim/after/queries/clojure/highlights.scm
@@ -1,1 +1,9 @@
 (kwd_lit) @constant
+
+;; def-like things
+(list_lit
+ .
+ (sym_lit) @function.macro
+ .
+ (sym_lit) @function
+ (#match? @function.macro "^(declare|def|definline|definterface|defmacro|defmethod|defmulti|defn|defn-|defonce|defprotocol|defstruct|deftype|ns)$"))
