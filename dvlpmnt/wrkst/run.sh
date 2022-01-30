baf459c91b82de25a0e74c2b0c6ce330b44e88ac
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-01-30 14:35:54 -0800

    feat(ws): update configs to be more general

--- dvlpmnt/wrkst/run.sh
+++ dvlpmnt/wrkst/run.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-ansible-playbook main.yml --ask-become -v
+ansible-playbook main.yml --ask-become -v --skip-tags "update"
---
+++ dvlpmnt/wrkst/update.sh
@@ -0,0 +1,1 @@
+ansible-playbook main.yml --ask-become -v --tags "update"
