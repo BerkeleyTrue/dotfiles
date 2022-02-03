ccc96b654eb56bbac8431051d665065880f66c57
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-02-03 11:21:45 -0800

    feat(shell/common_helpers): add kll (fzf kill)

--- .config/shell/common_helpers.sh
+++ .config/shell/common_helpers.sh
@@ -2,3 +2,17 @@
 source_shell() {
   source $XDG_CONFIG_HOME/shell/index.sh
 }
+
+kll() {
+  local pid
+  if [[ $# -eq 0 ]]; then
+    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}') || return
+  else
+    pid=$1
+  fi
+
+  if [[ -n "$pid" ]]; then
+    echo $pid | xargs kill -9
+    return
+  fi
+}
