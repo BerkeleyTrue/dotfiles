effd94e43797be0d158b842fd4e12afa54d53f8a
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-02-09 23:11:59 -0800

    fix(nvim/queries/fennel/locals): some tweaks

--- .config/nvim/queries/fennel/locals.scm
+++ .config/nvim/queries/fennel/locals.scm
@@ -1,16 +1,21 @@
 (list
   .
-  (symbol) @keyword (#match? @keyword "^(fn|lambda|hashfn|λ|let)$")) @scope
+  (symbol) @keyword @scope (#any-of? @keyword "let" "when-let" "if-let")
+  (array (symbol) @definition.var))
 
-; defonce/defonce-
-(list . (symbol) @function.macro (#match? @function.macro "defonce\-?$") . (symbol) @definition.var)
-
-; def/def-
-(list . (symbol) @function.macro (#match? @function.macro "^def\-?$") . (symbol) @definition.var)
-
-; defn/defn-
-(list
- .  (symbol) @function.macro (#match? @function.macro "^defn\-?$") ; defn macro
-    (symbol) @definition.function ; defined function symbol
-    (array (symbol)* @definition.parameter)) @scope ;  args
-
+; (list
+;   .
+;   (symbol) @keyword (#match? @keyword "^(fn|lambda|hashfn|λ|let)$")) @scope
+;
+; ; defonce/defonce-
+; (list . (symbol) @function.macro (#match? @function.macro "defonce\-?$") . (symbol) @definition.var)
+;
+; ; def/def-
+; (list . (symbol) @function.macro (#match? @function.macro "^def\-?$") . (symbol) @definition.var)
+;
+; ; defn/defn-
+; (list
+;  .  (symbol) @function.macro (#match? @function.macro "^defn\-?$") ; defn macro
+;     (symbol) @definition.function ; defined function symbol
+;     (array (symbol)* @definition.parameter)) @scope ;  args
+;
