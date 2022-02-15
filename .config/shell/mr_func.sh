86dc5fc5a10d4da3a35ce6df0bb7c34e843e69d6
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-02-14 22:45:07 -0800

    fix(shell): add mr funcs

---
+++ .config/shell/mr_func.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# add alias for MR on command
+mrenv() {
+  export NODE_PATH=~/dvlpmnt/node/mr/actual/mr_modules
+  export NODE_ENV=local
+}
+
+mrinit() {
+  mrenv
+  /usr/bin/docker start mr-mongo mr-mysql-sanitized mr_redis_1 > /dev/null
+}
+
+mrsd() {
+  /usr/bin/docker stop mr-mongo mr-mysql-sanitized mr_redis_1 > /dev/null
+}
+
+mrth() {
+  mrinit
+  npm run dev-tophat
+}
+
+mrcc() {
+  mrinit
+  npm run dev-cc
+}
+
+mrws() {
+  mrinit
+  npm run dev-website
+}
+
+mrpcm() {
+  mrinit
+  npm run dev-pcm
+}
+
+mrcctests() {
+  mrinit
+  cd ~/dvlpmnt/node/mr/actual/raven
+  npm run test
+}
+
+mrccstories() {
+  mrinit
+  cd ~/dvlpmnt/node/mr/actual/raven
+  npm run stories
+}
