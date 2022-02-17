04785fc24579e9dc6960610ad49c476c490c5d01
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-02-16 21:41:33 -0800

    fix(nvim/init): refactor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,34 @@
-local vim = _G.vim
 -- check if aniseed is installed, if not, run make aniseed to install and
+local vim = _G.vim
+local confpath = vim.fn.stdpath("config")
+local isInConf = confpath == vim.fn.getcwd()
 
--- ensure packer file
-os.execute('mkdir -p pack/packer')
--- make sure aniseed path is available for macros lookup
-vim.cmd [[ packadd aniseed ]]
-local isInConf = vim.fn.stdpath("config") == vim.fn.getcwd()
-
--- force aniseed to compile while in nvim dir (in dev mode)
 if isInConf then
-  print("[init.vim]: in conf dir: force compiling")
+  -- ensure packer path
+  os.execute('mkdir -p pack/packer')
+  print("[init.vim]: in conf dir")
 end
 
-local ok, res = pcall(require, 'aniseed.env')
+-- make sure aniseed path is available for macros lookup
+vim.cmd [[ packadd aniseed ]]
 
+local ok, anenv = pcall(require, 'aniseed.env')
+
+-- force aniseed to compile while in nvim dir (in dev mode)
 -- aniseed is available, compile and load
 if ok then
-  res.init({ force = isInConf })
+  print("[init.vim]: force compile")
+  anenv.init({ force = isInConf })
 else
   print('Aniseed not found. Running aniseed install now')
   print(vim.api.nvim_call_function('system', {'make aniseed'}))
-  local ok2, res2 = pcall(require, 'aniseed.env')
+  local ok, anenv = pcall(require, 'aniseed.env')
 
-  if not ok2 then
+  if not ok then
     print('Could not load after install')
   else
-    res2.init({ force = isInConf })
+    print("[init.vim]: compile")
+    anenv.init({ force = isInConf })
   end
 
 end
