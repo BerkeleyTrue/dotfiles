ea61320ceb37d328e9f8c3b8fc0b3d1c3f70c640
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-02-21 10:08:48 -0800

    feat(config/shell): add captainslog

--- .config/shell/task.sh
+++ .config/shell/task.sh
@@ -5,17 +5,17 @@ alias tadd='task add'
 
 tcon() {
   if [[ $# -eq 0 ]]; then
-    task context show;
-    return 0;
+    task context show
+    return 0
   fi
   eval "task context $*"
 }
 # tmod 14 proj:foo
 tmod() {
   if [[ $# -eq 0 ]]; then
-    echo "no arguments supplied";
+    echo "no arguments supplied"
     echo "usage: tmod <task-id> <modification> [, ...<modification>]"
-    return 1;
+    return 1
   fi
   local num=$1
   shift
@@ -24,18 +24,31 @@ tmod() {
 
 tdone() {
   if [[ $# -eq 0 ]]; then
-    echo "no arguments supplied";
+    echo "no arguments supplied"
     echo "usage: tdone <task-id>"
-    return 1;
+    return 1
   fi
   eval "task $1 done"
 }
 
 tedit() {
   if [[ $# -eq 0 ]]; then
-    echo "no arguments supplied";
+    echo "no arguments supplied"
     echo "usage: tedit <task-id>"
-    return 1;
+    return 1
   fi
   eval "task $1 edit"
+}
+
+captainslog() {
+  vim + "$(date +"%y-%m-%d %H:%M" | while read d t; do
+    f=$HOME/docs/notes/captainslog/$d.md
+    (
+      echo
+      echo \#\# $t
+      echo
+      echo
+    ) >>"$f"
+    echo "$f"
+  done)"
 }
