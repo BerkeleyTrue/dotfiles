7ccf9cc2753664152eb3a53339e79b689aaf5098
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-01 09:15:20 -0700

    fix(conf/shell/x11): add log alias

--- .config/shell/x11.sh
+++ .config/shell/x11.sh
@@ -1,11 +1,21 @@
+LOG=$HOME/.local/share/x11/x11.log
+
 startxmonad() {
   if [[ "$(tty)" = "/dev/tty1" ]]; then
-    pgrep X || startx $HOME/.config/x11/xinitrc >$HOME/.local/share/x11/x11.log 2>&1
+    pgrep X || startx $HOME/.config/x11/xinitrc >$LOG 2>&1
   fi
 }
 
 startplasma() {
   if [[ "$(tty)" = "/dev/tty1" ]]; then
-    pgrep X || startx $HOME/.config/x11/xinitrc kde >$HOME/.local/share/x11/x11.log 2>&1
+    pgrep X || startx $HOME/.config/x11/xinitrc kde >$LOG 2>&1
   fi
+}
+
+x11logtail() {
+  tail -f $LOG
+}
+
+x11logvim() {
+  nvim $LOG
 }
