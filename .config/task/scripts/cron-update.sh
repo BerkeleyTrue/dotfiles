50708c6a18df42a8773d514a7d90ad9d25db5391
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-16 12:20:46 -0700

    fix(conf/task/sync): better lock management

--- .config/task/scripts/cron-update.sh
+++ .config/task/scripts/cron-update.sh
@@ -2,15 +2,18 @@
 TASK_BACKLOG=~/.config/task/backlog.data
 TASK_SHARE=~/.local/share/task
 SYNC_LOG=$TASK_SHARE/cron-update.log
-LOCK_FILE=$TASK_SHARE/autosync.lock
+LOCK_FILE=$TASK_SHARE/SYNC.lock
 
-if [ ! -f $LOCK_FILE ]; then
-  touch $LOCK_FILE
+if {
+  # set C ensures redirect cannot overwrite a file.
+  set -C
+  # create lock file if it doesn't exist, pipe errors to /dev/null
+  2>/dev/null >$LOCK_FILE
+}; then
+  # trap on exit to remove lockfile
+  trap 'rm -f "$LOCK_FILE"' EXIT
 
   output=$(task sync)
   echo "$(date "+\%F \%T") - $output" >>$SYNC_LOG 2>&1
-
-  rm $LOCK_FILE
 fi
-
-exit 0
+exit
