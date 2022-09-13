29be8a3d0c7bd372a9ca87f59d386b135b608cb1
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-09-13 10:46:15 -0600

    feat(conf/xmonad/layouts/vert): flip initial orientation

--- .config/xmonad/src/Berks/Layouts/Vert.hs
+++ .config/xmonad/src/Berks/Layouts/Vert.hs
@@ -2,13 +2,18 @@ module Berks.Layouts.Vert
   ( vert
   ) where
 
-import qualified XMonad as X
-import qualified XMonad.Layout as L
-import qualified XMonad.Layout.LayoutModifier as LM
-import qualified XMonad.Layout.Renamed as RN
-import qualified XMonad.Layout.Spacing as Spacing
-import qualified XMonad.Layout.WindowNavigation as WN
+import qualified XMonad                        as X
+import qualified XMonad.Layout                 as L
+import qualified XMonad.Layout.LayoutModifier  as LM
+import           XMonad.Layout.Reflect         as LR
+import qualified XMonad.Layout.Renamed         as RN
+import qualified XMonad.Layout.Spacing         as Spacing
+import qualified XMonad.Layout.WindowNavigation
+                                               as WN
 
 vert =
-  RN.renamed [RN.Replace " \xf8de "] $
-  Spacing.smartSpacing 1 $ WN.windowNavigation $ L.Tall 1 (3 / 100) (1 / 2)
+  RN.renamed [RN.Replace " \xf8de "]
+    $ LR.reflectHoriz
+    $ Spacing.smartSpacing 1
+    $ WN.windowNavigation
+    $ L.Tall 1 (3 / 100) (1 / 2)
