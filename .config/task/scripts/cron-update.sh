ee79dde943e73368eef17f87e37f562c8a920e39
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-12-16 22:25:16 -0700

    fix(conf/task): update sync scripts
    use tmux session to debounce

--- .config/task/scripts/cron-update.sh
+++ .config/task/scripts/cron-update.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 TASK_BACKLOG=~/.config/task/backlog.data
 TASK_SHARE=~/.local/share/task
 SYNC_LOG=$TASK_SHARE/cron-update.log
-LOCK_FILE=$TASK_SHARE/SYNC.lock
+TMUX_SESSION=task-sync
+DELAY=10
 
-if {
-  # set C ensures redirect cannot overwrite a file.
-  set -C
-  # create lock file if it doesn't exist, pipe errors to /dev/null
-  2>/dev/null >$LOCK_FILE
-}; then
-  # trap on exit to remove lockfile
-  trap 'rm -f "$LOCK_FILE"' EXIT
-
-  output=$(task sync)
-  echo "$(date "+\%F \%T") - $output" >>$SYNC_LOG 2>&1
+if ! tmux has-session -t $TMUX_SESSION 2>/dev/null; then
+  echo "Stopping previous sync" >> $SYNC_LOG
 fi
+
+# first kill previous tmux sync session
+tmux kill-session -t $TMUX_SESSION 2>/dev/null
+
+# create tmux session, and start sync
+tmux new-session -d -s $TMUX_SESSION "sleep $DELAY && date \"+\%F \%T\" >> $SYNC_LOG && task sync >> $SYNC_LOG 2>&1\
+    || echo 'Sync failed' >> $SYNC_LOG"
+
 exit
