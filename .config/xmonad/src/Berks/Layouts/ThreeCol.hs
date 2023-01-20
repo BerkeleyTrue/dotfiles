7b4c17ffa35d4ffc1d16ae7ca81ee8808a322f56
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-01-19 19:48:56 -0700

    feat(conf/xmonad/layout/threecol): add larger borders

--- .config/xmonad/src/Berks/Layouts/ThreeCol.hs
+++ .config/xmonad/src/Berks/Layouts/ThreeCol.hs
@@ -6,19 +6,23 @@ where
 import XMonad
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.LimitWindows
+import XMonad.Layout.NoBorders
 import XMonad.Layout.Renamed
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
 
-type LimitedThreeCol = ModifiedLayout LimitWindows ThreeCol
-
-type SpacedThreeCol = ModifiedLayout Spacing LimitedThreeCol
-
-type ThreeColLayout = ModifiedLayout Rename SpacedThreeCol
+type ThreeColLayout =
+  ModifiedLayout
+    Rename
+    ( ModifiedLayout
+        WithBorder
+        (ModifiedLayout Spacing (ModifiedLayout LimitWindows ThreeCol))
+    )
 
 threeCol :: ThreeColLayout Window
 threeCol =
   renamed [Replace " ﰦ "] $
-    spacingRaw True (Border 4 4 0 6) True (Border 0 0 6 0) True $
-      limitWindows 7 $
-        ThreeColMid 1 (3 / 100) (1 / 2)
+    withBorder 4 $
+      spacingRaw True (Border 4 4 0 6) True (Border 0 0 6 0) True $
+        limitWindows 7 $
+          ThreeColMid 1 (3 / 100) (1 / 2)
