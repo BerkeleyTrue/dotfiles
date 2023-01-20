25df1cf0c43488f594c49eb43aa44b85d67a2644
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-01-19 19:28:02 -0700

    fix(conf/xmonad/layout/threecol): add type aliases

--- .config/xmonad/src/Berks/Layouts/ThreeCol.hs
+++ .config/xmonad/src/Berks/Layouts/ThreeCol.hs
@@ -3,11 +3,20 @@ module Berks.Layouts.ThreeCol
   )
 where
 
+import XMonad
+import XMonad.Layout.LayoutModifier
 import XMonad.Layout.LimitWindows
 import XMonad.Layout.Renamed
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
 
+type LimitedThreeCol = ModifiedLayout LimitWindows ThreeCol
+
+type SpacedThreeCol = ModifiedLayout Spacing LimitedThreeCol
+
+type ThreeColLayout = ModifiedLayout Rename SpacedThreeCol
+
+threeCol :: ThreeColLayout Window
 threeCol =
   renamed [Replace " ﰦ "] $
     spacingRaw True (Border 4 4 0 6) True (Border 0 0 6 0) True $
