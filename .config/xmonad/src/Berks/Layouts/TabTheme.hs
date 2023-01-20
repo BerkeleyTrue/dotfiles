e20842951fe877a46ed54442087d6b1d1fec8dc1
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-01-19 20:04:13 -0700

    chore(conf/xmonad/layouts/main): format

--- .config/xmonad/src/Berks/Layouts/TabTheme.hs
+++ .config/xmonad/src/Berks/Layouts/TabTheme.hs
@@ -1,32 +1,35 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Berks.Layouts.TabTheme
-  ( tabbedDecorator
-  ) where
+  ( tabbedDecorator,
+  )
+where
 
--- import qualified XMonad.Config.Prime as Prime
-import XMonad.Core
-import XMonad.Layout.Decoration hiding (LayoutModifier)
-import XMonad.Layout.Tabbed
+-- import XMonad.Config.Prime
 
 import Berks.Colors
 import Berks.Font
+import XMonad.Core
+import XMonad.Layout.Decoration hiding
+  ( LayoutModifier,
+  )
+import XMonad.Layout.Tabbed
 
 -- Layouts:
 tabTheme :: Theme
 tabTheme =
   def
-    { fontName = font
-    , inactiveTextColor = comment
-    , inactiveColor = background
-    , inactiveBorderColor = background
-    , activeTextColor = foreground
-    , activeColor = cyan
-    , activeBorderColor = cyan
+    { fontName = font,
+      inactiveTextColor = comment,
+      inactiveColor = background,
+      inactiveBorderColor = background,
+      activeTextColor = foreground,
+      activeColor = cyan,
+      activeBorderColor = cyan
     }
 
 tabbedDecorator ::
-     (Eq a, LayoutClass l a)
-  => l a
-  -> ModifiedLayout (Decoration TabbedDecoration DefaultShrinker) l a
+  (Eq a, LayoutClass l a) =>
+  l a ->
+  ModifiedLayout (Decoration TabbedDecoration DefaultShrinker) l a
 tabbedDecorator = addTabs shrinkText tabTheme
