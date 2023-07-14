8002aebd21dea8412c90cf84d071eb9c39a533e8
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-07-14 09:41:37 -0600

    fix(conf/nixpkgs/apps): rofi should include calc,emoji

--- .config/nixpkgs/mixins/apps.nix
+++ .config/nixpkgs/mixins/apps.nix
@@ -1,12 +1,21 @@
 { pkgs, ... }:
+let
+  rofi = pkgs.rofi.override {
+    plugins = with pkgs; [
+      rofi-calc
+      rofi-emoji
+    ];
+  };
+in
 {
-  home.packages = with pkgs; [
-    gparted # graphical partition manager
-    rofi # dmenu replacement
-    rofi-calc # calculator
-    viewnior # fast image preview
-    zathura # pdf viewer
-    keybase # encrypted chat
-    keybase-gui # encrypted chat
-  ];
+  home.packages = with pkgs;
+    [
+      gparted # graphical partition manager
+      rofi-calc # calculator
+      viewnior # fast image preview
+      zathura # pdf viewer
+      keybase # encrypted chat
+      keybase-gui # encrypted chat
+      rofi # launcher
+    ];
 }
