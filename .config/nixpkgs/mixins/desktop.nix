0ce189ad78a03a445bd0d3d3283017f1c310e63d
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-07-14 09:25:36 -0600

    fix(conf/nixpkgs/dunst): move to own file

--- .config/nixpkgs/mixins/desktop.nix
+++ .config/nixpkgs/mixins/desktop.nix
@@ -1,7 +1,10 @@
-{ pkgs, theme, ... }:
+{ pkgs, ... }:
 {
+  imports = [
+    ./desktop
+  ];
+
   home.packages = with pkgs; [
-    dunst # notification daemon
     flameshot # screenshot util
     nitrogen # wallpaper manager
     picom # X11 compositor
@@ -10,106 +13,4 @@
     xplugd # A UNIX daemon that executes a script on X input and RandR changes
     yad # A fork of Zenity with many improvements
   ];
-
-  services.dunst = {
-    enable = true;
-
-    iconTheme = {
-      name = "Dracula Theme";
-      package = pkgs.dracula-icon-theme;
-    };
-
-    settings = {
-      global = {
-        frame_width = 1;
-        frame_color = theme.colors.purple;
-        font = "FiraCode Nerd Font 10";
-        format = "<b>%a</b>: <i>%s</i> %p\n%b";
-        markup = "full";
-        sort = "no";
-        indicate_hidden = "yes";
-        alignment = "left";
-        show_age_threshold = 60;
-        word_wrap = "yes";
-        ellipsize = "end";
-        corner_radius = 8;
-        mouse_left_click = "close_current";
-        mouse_right_click = "do_action";
-        ignore_newline = "no";
-        width = "(200, 400)";
-        height = 400;
-        offset = "10x30";
-        shrink = "yes";
-        transparency = 15;
-        idle_threshold = 60;
-        monitor = 0;
-        follow = "none";
-        sticky_history = true;
-        history_length = 100;
-
-        # Display indicators for URLs (U) and actions (A).
-        show_indicators = false;
-
-        # The height of a single line.  If the height is smaller than the
-        # font height, it will get raised to the font height.
-        # This adds empty space above and under the text.
-        line_height = 1;
-
-        # Draw a line of "separator_height" pixel height between two
-        # notifications.
-        # Set to 0 to disable.
-        separator_height = 1;
-
-        # Padding between text and separator.
-        # padding = 8
-        padding = 8;
-
-        # Horizontal padding.
-        horizontal_padding = 10;
-
-        # Define a color for the separator.
-        # possible values are:
-        #  * auto: dunst tries to find a color fitting to the background;
-        #  * foreground: use the same color as the foreground;
-        #  * frame: use the same color as the frame;
-        #  * anything else will be interpreted as a X color.
-        separator_color = "#6272a4";
-
-        # dmenu path.
-        dmenu = "${pkgs.rofi} -dmenu -p dunst:";
-
-        # Browser for opening urls in context menu.
-        browser = "org.mozilla.firefox";
-
-
-        # Align icons left/right/off
-        icon_position = "left";
-
-        # Paths to default icons.
-        # icon_path = /usr/share/icons/Papirus/48x48/devices/:/usr/share/icons/Papirus/48x48/status/:/usr/share/icons/Papirus/48x48/apps/
-
-        # Limit icons size.
-        max_icon_size = 100;
-      };
-
-      urgency_low = {
-        background = theme.colors.background;
-        foreground = theme.colors.foreground;
-        timeout = 10;
-      };
-
-      urgency_normal = {
-        background = theme.colors.selection;
-        foreground = theme.colors.foreground;
-        timeout = 10;
-      };
-
-      urgency_critical = {
-        background = theme.colors.red;
-        foreground = theme.colors.background;
-        frame_color = theme.colors.red;
-        timeout = 0;
-      };
-    };
-  };
 }
