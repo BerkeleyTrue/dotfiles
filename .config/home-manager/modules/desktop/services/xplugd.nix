9f3dcf8e00f5f14dd79af436fd87879250edce51
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-04 22:15:39 -0600

    fix(conf/home-manager/services/xplugd): add rc

--- .config/home-manager/modules/desktop/services/xplugd.nix
+++ .config/home-manager/modules/desktop/services/xplugd.nix
@@ -1,4 +1,19 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  rc = pkgs.writeShellScript "xplugrc" ''
+
+    if [ "$1" == "display" ]; then
+      $HOME/.config/screenlayout/default.sh
+      exit 0
+    fi
+
+    case "$1,$3,$4" in
+    keyboard,*,*)
+      systemd --user restart xkbmap.service
+      ;;
+    esac
+    exit 0
+  '';
+in {
   systemd.user.services.xplugd = {
     Unit = {
       Description = "Rerun after IO has changed using xplugd";
@@ -8,7 +23,7 @@
 
     Service = {
       Type = "forking";
-      ExecStart = "${pkgs.xplugd}/bin/xplugd";
+      ExecStart = "${pkgs.xplugd}/bin/xplugd ${rc}";
     };
 
     Install = {
