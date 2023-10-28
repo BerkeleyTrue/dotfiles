3577d2e1105d8ac5ab3b780eede5cff7f079891e
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-10-28 15:26:02 -0600

    fix(conf/home-manager): add catppuccin to bat

--- .config/home-manager/modules/commandline.nix
+++ .config/home-manager/modules/commandline.nix
@@ -28,6 +28,13 @@
 
     _get_git_folders "$@"
   '';
+
+  bat-catppucchin = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "bat";
+    rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+    sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+  };
 in {
   home.packages = with pkgs; [
     antigen # zsh plugin manager
@@ -104,6 +111,23 @@ in {
     # A cat(1) clone with wings
     bat = {
       enable = true;
+
+      config = {
+        theme = "catppuccin";
+        map-syntax = [
+          "*.fnl:Clojure"
+          "*.templ:Go"
+          "flake.lock:JSON"
+        ];
+      };
+
+      themes = {
+        catppuccin = {
+          src = bat-catppucchin;
+          file = "/Catppuccin-frappe.tmTheme";
+        };
+      };
+
       extraPackages = with pkgs.bat-extras; [
         batman
         batpipe
