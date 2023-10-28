e38c7392e4742b1307ad5cff0b83b20ff0a4b3d8
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-10-28 17:39:26 -0600

    fix(conf/home-manager): add xdg_data_dirs to systemd
    Needs to be set in .config/systemd/user.conf because processes launched
    by systemd won't pick it up in the environment

--- .config/home-manager/modules/desktop/services/default.nix
+++ .config/home-manager/modules/desktop/services/default.nix
@@ -17,4 +17,12 @@
     ./xkbmap.nix
     ./xplugd.nix
   ];
+
+
+  # needed to load env vars for systemd user services
+  xdg.configFile."systemd/user.conf".text = ''
+    [Manager]
+    ManagerEnvironment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:/usr/local/share:/usr/share"
+    DefaultEnvironment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/.share:/usr/local/share:/usr/share"
+  '';
 }
