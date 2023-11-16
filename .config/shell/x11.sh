c7613f62860c0ccdbc56f02a278acb12a2f56127
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-11-15 18:38:08 -0700

    fix(x11/startxmonad): got everything working flawlessly

--- .config/shell/x11.sh
+++ .config/shell/x11.sh
@@ -1,12 +1,5 @@
 LOG=$HOME/.local/share/x11/x11.log
 
-startxmonad() {
-  systemctl --user import-environment PATH DBUS_SESSION_BUS_ADDRESS
-  if [[ "$(tty)" = "/dev/tty1" ]]; then
-    pgrep -x Xorg || startx $HOME/.config/x11/xinitrc &>$LOG
-  fi
-}
-
 startplasma() {
   if [[ "$(tty)" = "/dev/tty1" ]]; then
     pgrep -x Xorg || startx $HOME/.config/x11/xinitrc kde &>$LOG
