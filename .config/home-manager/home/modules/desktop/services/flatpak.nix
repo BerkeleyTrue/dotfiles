1f6843dedd2b5df6003728f0fc903a7456e2f7bc
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-03-22 23:58:02 -0600

    feat(home-manager): add rest of flatpaks

--- .config/home-manager/home/modules/desktop/services/flatpak.nix
+++ .config/home-manager/home/modules/desktop/services/flatpak.nix
@@ -1,8 +1,28 @@
-{pkgs, ...}: {
+{...}: {
   services.flatpak = {
     enable = true;
     packages = [
-      "com.uploadedlobster.peek"
+      "app.ytmdesktop.ytmdesktop"           
+      "com.discordapp.Discord"              
+      "com.github.geigi.cozy"               
+      "com.github.libresprite.LibreSprite"  
+      "com.github.tchx84.Flatseal"          
+      "com.github.wwmm.pulseeffects"        
+      "com.ultimaker.cura"                  
+      "com.uploadedlobster.peek"            
+      "com.vscodium.codium"                 
+      "io.dbeaver.DBeaverCommunity"         
+      "io.github.f3d_app.f3d"               
+      "org.chromium.Chromium"               
+      "org.duckstation.DuckStation"         
+      "org.freecadweb.FreeCAD"              
+      "org.gimp.GIMP"                       
+      "org.inkscape.Inkscape"               
+      "org.kde.krita"                       
+      "org.libreoffice.LibreOffice"         
+      # "org.mozilla.firefox"
+      "org.pipewire.Helvum"                 
+      "org.standardnotes.standardnotes"     
     ];
   };
 }
