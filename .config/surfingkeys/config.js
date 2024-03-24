43d7f484c8da4ef262bb5dd43e6391d770e54666
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-03-24 15:12:57 -0600

    fix(surfingkeys): update to frappe

--- .config/surfingkeys/config.js
+++ .config/surfingkeys/config.js
@@ -1,30 +1,30 @@
 const palette = {
-  rosewater: "#f5e0dc",
-  flamingo: "#f2cdcd",
-  pink: "#f5c2e7",
-  mauve: "#cba6f7",
-  red: "#f38ba8",
-  maroon: "#eba0ac",
-  peach: "#fab387",
-  yellow: "#f9e2af",
-  green: "#a6e3a1",
-  teal: "#94e2d5",
-  sky: "#89dceb",
-  sapphire: "#74c7ec",
-  blue: "#87b0f9",
-  lavender: "#b4befe",
+  rosewater: "#f2d5cf",
+  flamingo: "#eebebe",
+  pink: "#f4b8e4",
+  mauve: "#ca9ee6",
+  red: "#e78284",
+  maroon: "#ea999c",
+  peach: "#ef9f76",
+  yellow: "#e5c890",
+  green: "#a6d189",
+  teal: "#81c8be",
+  sky: "#99d1db",
+  sapphire: "#85c1dc",
+  blue: "#8caaee",
+  lavender: "#babbf1",
   text: "#c6d0f5",
-  subtext1: "#b3bcdf",
-  subtext0: "#a1a8c9",
-  overlay2: "#8e95b3",
-  overlay1: "#7b819d",
-  overlay0: "#696d86",
-  surface2: "#565970",
-  surface1: "#43465a",
-  surface0: "#313244",
-  base: "#1e1e2e",
-  mantle: "#181825",
-  crust: "#11111b",
+  subtext1: "#b5bfe2",
+  subtext0: "#a5adce",
+  overlay2: "#949cbb",
+  overlay1: "#838ba7",
+  overlay0: "#737994",
+  surface2: "#626880",
+  surface1: "#51576d",
+  surface0: "#414559",
+  base: "#303446",
+  mantle: "#292c3c",
+  crust: "#232634",
 };
 
 function convertPaletteToCSS(palette) {
@@ -483,11 +483,12 @@ div.sk_arrow[dir=up]>div:nth-of-type(2) {
 `;
 
 api.Hints.style(`
-  font-size: 16px;
-  border: none;
-  color: ${palette.rosewater};
+  font-size: 10px;
+  border: 1px solid ${palette.flamingo};
+  padding: 1px;
+  color: ${palette.sapphire};
   background: none; 
-  background-color: #181825;
+  background-color: ${palette.overlay1};
 `);
 
 api.map("H", "S");
