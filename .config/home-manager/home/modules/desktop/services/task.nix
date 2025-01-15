58383455a00330972638442caa674176e36f258e
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-01-15 00:48:38 -0800

    fix(home-manager): update task syncing to use tw3

--- .config/home-manager/home/modules/desktop/services/task.nix
+++ .config/home-manager/home/modules/desktop/services/task.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: let
-  sync = pkgs.writeScriptBin "sync-tasks" ''
-    #!/bin/bash
+  sync = pkgs.writeShellScriptBin "sync-tasks" ''
     TMUX_SESSION=__task-sync__
     DELAY=10
     TASK_SHARE=~/.local/share/task
@@ -14,14 +13,16 @@
     tmux kill-session -t $TMUX_SESSION 2>/dev/null
 
     # create tmux session, and start sync
-    tmux new-session -d -s $TMUX_SESSION "sleep $DELAY && date \"+\%F \%T\" >> $SYNC_LOG && task sync >> $SYNC_LOG 2>&1 || echo 'Sync failed'" >> $SYNC_LOG
+    tmux new-session -d -s $TMUX_SESSION "sleep $DELAY && date \"+%F %T\" >> $SYNC_LOG && task sync >> $SYNC_LOG 2>&1 || echo 'Sync failed'" >> $SYNC_LOG
     cat $SYNC_LOG
+    # clear sync log
+    echo "" > $SYNC_LOG
     exit
   '';
 in {
   systemd.user.services.task-sync = {
     Unit = {
-      Description = "Run Taskwarrior sync";
+      Description = "Run Taskwarrior Sync";
     };
 
     Service = {
@@ -30,31 +31,31 @@ in {
     };
   };
 
-  # systemd.user.timers.task-sync = {
-  #   Unit = {
-  #     Description = "Run Taskwarrior sync periodically";
-  #   };
-  #
-  #   Timer = {
-  #     OnBootSec = 80;
-  #     OnCalendar = "*:0/10"; # run every 10 minutes
-  #   };
-  #
-  #   Install = {
-  #     WantedBy = ["timers.target"];
-  #   };
-  # };
+  systemd.user.timers.task-sync = {
+    Unit = {
+      Description = "Run Taskwarrior sync periodically";
+    };
+
+    Timer = {
+      OnBootSec = 80;
+      OnCalendar = "*:0/10"; # run every 10 minutes
+    };
+
+    Install = {
+      WantedBy = ["timers.target"];
+    };
+  };
 
   systemd.user.services.task-alert-overdue = {
     Unit = {
-      Description = "Run taskwarrior alert";
+      Description = "Run Taskwarrior Alert";
     };
 
     Service = {
       Type = "oneshot";
       ExecStart = pkgs.writeShellScript "taskwarrior-alert" ''
         set -euo pipefail
-        count=$(${pkgs.taskwarrior}/bin/task +OVERDUE count)
+        count=$(${pkgs.taskwarrior3}/bin/task +OVERDUE count)
         # if there are no overdue tasks, do nothing
         if [ $count -eq 0 ]; then
           echo "No overdue tasks, exiting..."
