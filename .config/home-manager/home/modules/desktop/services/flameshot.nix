c3dc17b69308856ddce4b59ebfcd521f5a79d217
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-02 11:51:43 -0700

    fix(hm/flameshot): use builtin services

--- .config/home-manager/home/modules/desktop/services/flameshot.nix
+++ .config/home-manager/home/modules/desktop/services/flameshot.nix
@@ -3,28 +3,7 @@
     pkgs.flameshot
   ];
 
-  systemd.user.services.flameshot = {
-    Unit = {
-      Description = "Flameshot screenshot tool";
-      Documentation = ["man:flameshot(1)"];
-      After = ["tray.target" "xmonad-session.target"];
-      PartOf = ["tray.target"];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.flameshot}/bin/flameshot";
-      Restart = "on-abort";
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      NoNewPrivileges = true;
-      PrivateUsers = true;
-      RestrictNamespaces = true;
-      SystemCallArchitectures = "native";
-      SystemCallFilter = "@system-service";
-    };
-
-    Install = {
-      WantedBy = ["tray.target"];
-    };
+  services.flameshot = {
+    enable = true;
   };
 }
