43055d1613d2e3e3f86fb1f320db223e48860247
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-02 14:18:19 -0700

    fix(hm/taffybar): stop before x11
    Still seems to exit code 1, 🤷

--- .config/home-manager/home/modules/desktop/services/taffybar.nix
+++ .config/home-manager/home/modules/desktop/services/taffybar.nix
@@ -3,8 +3,12 @@
     Unit = {
       Description = "Taffybar Status Bar";
       PartOf = ["tray.target"];
-      BindsTo = ["x11-session.target"];
-      ConditionEnvironment = "XDG_SESSION_TYPE=x11";
+      # we want taffybar to stop before x11-session so it cleanly shuts down.
+      # We start x11-session with xmonad.target in xinit so X server should alread be running.
+      # xmonad.target -> x11-session.target, which starts grapical-session.target, which starts tray.target
+      Before = ["x11-session.target"]; # Start before X11 session stops
+      BindsTo = ["x11-session.target"]; # Stop when X11 session stops
+      # ConditionEnvironment = "XDG_SESSION_TYPE=x11";
     };
 
     Service = {
