86f61d4c006009aaed9beee3b1807bbafddd533d
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-03 12:59:08 -0700

    feat(hm/desktop): init niri services

--- .config/home-manager/home/modules/desktop/niri/default.nix
+++ .config/home-manager/home/modules/desktop/niri/default.nix
@@ -1,5 +1,23 @@
-{kdl, ...}: let
+{
+  pkgs,
+  kdl,
+  ...
+}: let
   config = import ./config.nix {inherit kdl;};
 in {
+  home.packages = with pkgs; [
+    niri
+  ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+    configPackages = with pkgs; [
+      niri
+    ];
+  };
+
   xdg.configFile."niri/config.kdl".text = kdl.serialize.nodes config;
 }
