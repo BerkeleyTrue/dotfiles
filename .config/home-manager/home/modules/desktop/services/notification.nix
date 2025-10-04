c7e9054527798868867c9e0baf899f05664d1e7c
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-04 15:41:00 -0700

    fix(hm/services): first try at notification wrapper

---
+++ .config/home-manager/home/modules/desktop/services/notification.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  wrapper = pkgs.writeShellScript "notification-wrapper" ''
+    if [ -n "$WAYLAND_DISPLAY" ]; then
+      exec ${pkgs.mako}/bin/mako
+    elif [ -n "$DISPLAY" ]; then
+      exec ${pkgs.dunst}/bin/dunst -config ${config.xdg.configHome}/dunst/dunstrc
+    else
+      echo "No display server detected" >&2
+      exit 1
+    fi
+  '';
+in {
+  systemd.user.services.notification-daemon = {
+    Unit = {
+      Description = "Notification daemon (dunst/mako wrapper)";
+      Documentation = ["man:dunst(1)" "man:mako(1)"];
+      PartOf = ["notification.target"];
+    };
+
+    Service = {
+      Type = "dbus";
+      BusName = "org.freedesktop.Notifications";
+      ExecStart = "${wrapper}";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
+}
