2e9a0ac72a75afafb77b4832b24442b1b64b0b96
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-05 20:18:32 -0700

    feat(hm/desktop): add anyrun conf

--- .config/home-manager/home/modules/desktop/default.nix
+++ .config/home-manager/home/modules/desktop/default.nix
@@ -1,7 +1,20 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./services
     ./niri
     ./dunst.nix
   ];
+
+  programs.anyrun = {
+    enable = true;
+    config = {
+      plugins = [
+        "${pkgs.anyrun}/lib/libapplication.so"
+        "${pkgs.anyrun}/lib/libsymbols.so"
+        "${pkgs.anyrun}/lib/librink.so"
+        "${pkgs.anyrun}/lib/libdictionary.so"
+        "${pkgs.anyrun}/lib/libniri_focus.so"
+      ];
+    };
+  };
 }
