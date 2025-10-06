deeae73b3d20df017c3e59a60c58b2c50eaf02ed
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-06 11:16:43 -0700

    fix(hm/anyrun): move to own file

--- .config/home-manager/home/modules/desktop/default.nix
+++ .config/home-manager/home/modules/desktop/default.nix
@@ -1,21 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./services
     ./niri
     ./dunst.nix
     ./waybar.nix
+    ./anyrun.nix
   ];
-
-  programs.anyrun = {
-    enable = true;
-    config = {
-      plugins = [
-        "${pkgs.anyrun}/lib/libapplication.so"
-        "${pkgs.anyrun}/lib/libsymbols.so"
-        "${pkgs.anyrun}/lib/librink.so"
-        "${pkgs.anyrun}/lib/libdictionary.so"
-        "${pkgs.anyrun}/lib/libniri_focus.so"
-      ];
-    };
-  };
 }
