d23050328596561e23959ba404116a6ed70b312b
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-10-06 11:16:43 -0700

    fix(hm/anyrun): move to own file

---
+++ .config/home-manager/home/modules/desktop/anyrun.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  programs.anyrun = {
+    enable = true;
+    config = {
+      plugins = [
+        "${pkgs.anyrun}/lib/libapplication.so"
+        "${pkgs.anyrun}/lib/libsymbols.so"
+        "${pkgs.anyrun}/lib/librink.so"
+        "${pkgs.anyrun}/lib/libdictionary.so"
+        "${pkgs.anyrun}/lib/libniri_focus.so"
+      ];
+    };
+  };
+}
