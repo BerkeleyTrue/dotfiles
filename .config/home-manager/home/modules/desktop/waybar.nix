b51d92991af03095e2566f5d7f826e52f060483a
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-12-24 02:09:16 -0800

    fix(hm/sysd/waybar): update missing after/bind service

--- .config/home-manager/home/modules/desktop/waybar.nix
+++ .config/home-manager/home/modules/desktop/waybar.nix
@@ -5,6 +5,7 @@
     settings = {
       main = {
         layer = "overlay";
+        height = 22;
         modules-left = ["clock" "niri/workspaces"];
         modules-center = ["niri/window"];
         modules-right = ["pulseaudio" "network" "cpu" "memory"];
