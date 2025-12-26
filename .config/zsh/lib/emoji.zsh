340c2f9d1b355ff8eea56c2523d5cb508d2ad33e
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-12-25 18:10:09 -0800

    fix(zsh): make glyphs always one char wide

--- .config/zsh/lib/emoji.zsh
+++ .config/zsh/lib/emoji.zsh
@@ -1,36 +1,38 @@
 ghanima::emoji() {
   local name="$1"
 
+  # wrap in %1{} to ensure single width in zsh prompt
+  # otherwise it renders weirdly with in wayland
   if [[ $name == "nix" ]] then
-    echo " "
+    echo "%1{%}"
   elif [[ $name == "nodejs" ]] then
-    echo "󰎙"
+    echo "%1{󰎙%}"
   elif [[ $name == "div" ]] then
-    echo ""
+    echo "%1{%}"
   elif [[ $name == "divi" ]] then
-    echo ""
+    echo "%1{%}"
   elif [[ $name == "rdiv" ]] then
-    echo ""
+    echo "%1{%}"
   elif [[ $name == "rdivi" ]] then
-    echo ""
+    echo "%1{%}"
   elif [[ $name == "cir" ]] then
-    echo ""
+    echo "%1{%}"
   elif [[ $name == "rcir" ]] then
-    echo ""
+    echo "%1{%}"
   elif [[ $name == "plusminus" ]] then
-    echo "󱓊 "
+    echo "%1{󱓊%}"
   elif [[ $name == "branch" ]] then
-    echo "󰘬"
+    echo "%1{󰘬%}"
   elif [[ $name == "detached" ]] then
-    echo "󱓌 "
+    echo "%1{󱓌%}"
   elif [[ $name == "bomb" ]] then
-    echo " "
+    echo "%1{%}"
   elif [[ $name == "lightning" ]] then
-    echo "󱐋"
+    echo "%1{󱐋%}"
   elif [[ $name == "gear" ]] then
-    echo " "
+    echo "%1{%}"
   elif [[ $name == "delta" ]] then
-    echo "󰚌 "
+    echo "%1{󰚌%}"
   elif [[ $name == "block" ]] then
     echo "\e[1 q"
   elif [[ $name == "beam" ]] then
