c8f98ff938652a75b0e31af0834dc522160171da
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-12-26 01:21:36 -0800

    fix(hm/waybar): add battery icons

--- .config/home-manager/home/modules/desktop/waybar.nix
+++ .config/home-manager/home/modules/desktop/waybar.nix
@@ -12,15 +12,42 @@ in {
         modules-right = ["battery" "cpu" "memory" "tray"];
 
         clock.format = " {:%a %b %d | Week %V   %H:%M:%S}";
+        clock.interval = 1;
+        tray.spacing = 8;
+        battery = {
+          states = {
+            good = 80;
+            warning = 30;
+            critical = 20;
+          };
+          interval = 1;
+          tooltip = true;
+          format = "{icon} {capacity}%";
+          format-chargin = "<b>{icon}󱐋 {capacity}%</b>";
+          format-icons = [
+            "󰁺"
+            "󰁻"
+            "󰁼"
+            "󰁽"
+            "󰁾"
+            "󰁿"
+            "󰂀"
+            "󰂁"
+            "󰂂"
+            "󰁹"
+          ];
+        };
       };
     };
 
     style = ''
       window#waybar {
         padding: 0px 10px;
+        margin: 0px 15px;
         background: ${c.base};
         color: ${c.text};
       }
+      @import "./style2.css";
     '';
   };
 }
