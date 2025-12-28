0f3a648ccebea5fb92b22542443bf230b44817c4
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-12-28 00:30:44 -0800

    fix(hm/waybar): finish up modules, styling

--- .config/home-manager/home/modules/desktop/waybar.nix
+++ .config/home-manager/home/modules/desktop/waybar.nix
@@ -1,29 +1,32 @@
-{theme, ...}: let
-  c = theme.colors;
-in {
-  catppuccin.waybar.enable = true;
+{lib, ...}: {
   programs.waybar = {
     enable = true;
     settings = {
       main = {
         layer = "bottom";
         modules-left = ["niri/workspaces"];
-        modules-center = ["clock"];
-        modules-right = ["battery" "cpu" "memory" "tray"];
+        modules-center = ["clock#clock2" "custom/separator" "clock" "custom/separator" "custom/wakatime"];
+        modules-right = lib.intersperse "custom/separator" ["cpu" "temperature" "battery" "memory" "disk" "tray"];
 
-        clock.format = " {:%a %b %d | Week %V   %H:%M:%S}";
+        "clock#clock2" = {
+          format = " {:%a %b %d}";
+          interval = 60;
+        };
+        clock.format = "Week {:%V  %H:%M:%S}";
         clock.interval = 1;
-        tray.spacing = 8;
+
+        tray.spacing = 4;
+
         battery = {
           states = {
-            good = 80;
+            good = 100;
             warning = 30;
-            critical = 20;
+            danger = 20;
           };
           interval = 1;
           tooltip = true;
-          format = "{icon} {capacity}%";
-          format-chargin = "<b>{icon}󱐋 {capacity}%</b>";
+          format = "{icon}{capacity}%";
+          format-charging = "<b>󰂄{capacity}%</b>";
           format-icons = [
             "󰁺"
             "󰁻"
@@ -37,17 +40,70 @@ in {
             "󰁹"
           ];
         };
+
+        cpu = {
+          interval = 1;
+          format = " {usage}%";
+          states = {
+            danger = 90;
+            warning = 75;
+          };
+        };
+
+        temperature = {
+          interval = 1;
+          thermal-zone = 4;
+          warning-threshold = 75;
+          critical-threshold = 85;
+          format = "{icon}{temperatureC}°C";
+          format-icons = [
+            ""
+            ""
+            ""
+          ];
+        };
+
+        memory = {
+          interval = 1;
+          format = "{percentage}%";
+          states = {
+            danger = 90;
+            warning = 75;
+          };
+        };
+
+        disk = {
+          interval = 60;
+          # use letter spacing here to get some distance between icon and number
+          format = "󰉉 {specific_free:.0f}G";
+          unit = "GB";
+        };
+
+        "niri/workspaces" = {
+          format = "{icon}";
+          format-icons = {
+            "1" = "󰇊";
+            "2" = "󰇋";
+            "3" = "󰇌";
+            "4" = "󰇍";
+            "5" = "󰇎";
+            "6" = "󰇏";
+          };
+        };
+
+        "custom/separator" = {
+          format = "|";
+        };
+
+        "custom/wakatime" = {
+          interval = 10;
+          format = " {text}";
+          exec = "wakatoday";
+        };
       };
     };
 
-    style = ''
-      window#waybar {
-        padding: 0px 10px;
-        margin: 0px 15px;
-        background: ${c.base};
-        color: ${c.text};
-      }
-      @import "./style2.css";
-    '';
+    # use style file directly for faster iteration
+    style = null;
   };
 }
