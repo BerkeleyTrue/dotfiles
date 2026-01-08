a2dd0698e40b9610935807bf6cf87df62495aaf5
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-01-07 20:12:36 -0800

    fix(hm/desktop): set file terminal for portals

--- .config/home-manager/home/modules/desktop/niri/default.nix
+++ .config/home-manager/home/modules/desktop/niri/default.nix
@@ -31,12 +31,24 @@ in {
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
+      xdg-desktop-portal-termfilechooser
       xdg-desktop-portal-gtk
     ];
+    config.common = {
+      "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+    };
     configPackages = [
       niri
     ];
   };
 
   xdg.configFile."niri/config.kdl".source = validate-config (kdl.serialize.nodes niri-config);
+  xdg.configFile."xdg-desktop-portal-termfilechooser/config".text = ''
+    [filechooser]
+    cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+    env=TERMCMD=kitty --class "terminal-file-picker"
+    default_dir=$HOME
+    open_mode=suggested
+    save_mode=last
+  '';
 }
