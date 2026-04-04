be2b1539895c419b37c4863632efc615ebf14b66
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-04-03 22:41:39 -0700

    fix(hm): remove swaync override, remove nodePackages

--- .config/home-manager/home/modules/neovim.nix
+++ .config/home-manager/home/modules/neovim.nix
@@ -28,41 +28,38 @@
 in {
   programs.neovim = {
     enable = true;
-    extraPackages = with pkgs;
-      [
-        parinfer-rust
-        tree-sitter # to build grammars from source
-        nodejs_24
+    extraPackages = with pkgs; [
+      parinfer-rust
+      tree-sitter # to build grammars from source
+      nodejs_24
 
-        lua-language-server # A language server for Lua
-        nil #nix language server
-        nixd # nix language server
-        clojure-lsp
-        slint-lsp
-        rust-analyzer # rust language server
-        dockerfile-language-server # docker language server
-        basedpyright # python language server
+      lua-language-server # A language server for Lua
+      nil #nix language server
+      nixd # nix language server
+      clojure-lsp
+      slint-lsp
+      rust-analyzer # rust language server
+      dockerfile-language-server # docker language server
+      basedpyright # python language server
 
-        alejandra # formatting nix
-        shfmt # formatting shell scripts
-        rustfmt # formatting rust
+      alejandra # formatting nix
+      shfmt # formatting shell scripts
+      rustfmt # formatting rust
 
-        golines # formatting long lines in go
-        templ # formatting go templ files
-        gopls # Go language server
-      ]
-      ++ (with nodePackages; [
-        yaml-language-server # A language server for YAML
-        bash-language-server # A language server for Bash
-        typescript-language-server # TypeScript & JavaScript Language Server
-        vscode-langservers-extracted # Language servers extracted from VSCode, css mainly
-        tailwindcss-language-server # Language server for Tailwind CSS
-        vim-language-server # Language server for Vim script
+      golines # formatting long lines in go
+      templ # formatting go templ files
+      gopls # Go language server
 
-        prettier # for formatting js, ts, css, html, json, yaml, markdown
-        # purs-tidy # formatting purescript (not in nixpkgs anymore)
-        sql-formatter # formatting sql
-      ]);
+      prettier # for formatting js, ts, css, html, json, yaml, markdown
+      sql-formatter # formatting sql
+      vim-language-server # Language server for Vim script
+
+      yaml-language-server # A language server for YAML
+      bash-language-server # A language server for Bash
+      typescript-language-server # TypeScript & JavaScript Language Server
+      vscode-langservers-extracted # Language servers extracted from VSCode, css mainly
+      tailwindcss-language-server # Language server for Tailwind CSS
+    ];
 
     plugins = with pkgs.vimPlugins; [
       parinfer-rust
