5fd6a4f84ad2dc67a0ee954b15327da70b893cbe
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-04-11 13:27:41 -0700

    feat(hm/awww): move to dentrite

--- .config/home-manager/home/modules/desktop/services/awww.nix
+++ .config/home-manager/home/modules/desktop/services/awww.nix
@@ -77,29 +77,6 @@ in {
       width = 2256;
     };
   };
-  home.packages = [awww];
-
-  systemd.user.services.awww = {
-    Unit = {
-      Description = "awww wallpaper daemon";
-      ConditionEnvironment = "WAYLAND_DISPLAY";
-      After = ["graphical-session.target"];
-      PartOf = ["graphical-session.target"];
-    };
-
-    Service = {
-      ExecStart = "${lib.getExe' awww "awww-daemon"}";
-      Environment = [
-        "PATH=$PATH:${lib.makeBinPath [awww]}"
-      ];
-      Restart = "always";
-      RestartSec = 10;
-    };
-
-    Install = {
-      WantedBy = ["graphical-session.target"];
-    };
-  };
 
   systemd.user.services.set-wallpaper = {
     Unit = {
---
+++ .config/home-manager/modules/awww.nix
@@ -0,0 +1,37 @@
+{
+  inputs,
+  lib,
+  ...
+}: {
+  flake.modules.homeManager.desktop = {pkgs, ...}: let
+    awww = pkgs.awww;
+  in {
+    nixpkgs.overlays = [
+      inputs.awww.overlays.default
+    ];
+
+    packages = [awww];
+
+    systemd.user.services.awww = {
+      Unit = {
+        Description = "awww wallpaper daemon";
+        ConditionEnvironment = "WAYLAND_DISPLAY";
+        After = ["graphical-session.target"];
+        PartOf = ["graphical-session.target"];
+      };
+
+      Service = {
+        ExecStart = "${lib.getExe' awww "awww-daemon"}";
+        Environment = [
+          "PATH=$PATH:${lib.makeBinPath [awww]}"
+        ];
+        Restart = "always";
+        RestartSec = 10;
+      };
+
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+    };
+  };
+}
