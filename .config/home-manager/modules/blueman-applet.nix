7a3e8455765a97515b1eeea754de477c2f95d9d4
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-04-13 13:54:27 -0700

    refactor(hm): move blueman to modules

---
+++ .config/home-manager/modules/blueman-applet.nix
@@ -0,0 +1,28 @@
+{
+  flake.modules.homeModules.blueman = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      blueman
+    ];
+
+    # requires system installation of blueman
+    systemd.user.services.blueman-applet = {
+      Unit = {
+        Description = "Blueman Bluetooth Applet";
+        Documentation = ["man:blueman-applet(1)"];
+        After = ["tray.target"];
+        PartOf = ["tray.target"];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.blueman}/bin/blueman-applet";
+        Restart = "on-failure";
+        RestartSec = 3;
+      };
+
+      Install = {
+        WantedBy = ["tray.target"];
+      };
+    };
+  };
+}
