9854a5532e2fb78bd82d5743c8a1adfd9bcef7b6
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-04-13 12:40:21 -0700

    refactor(hm/cli): move to modules

---
+++ .config/home-manager/modules/cli-tools.nix
@@ -0,0 +1,227 @@
+{
+  flake.modules.homeModules.cli-tools = {
+    pkgs,
+    lib,
+    ...
+  }: let
+    getOptions = pkgs.getoptions.overrideAttrs (old: {
+      doCheck = false; # Will break on yash otherwise
+    });
+
+    projects = pkgs.writeShellApplication {
+      name = "projects";
+      runtimeInputs = [
+        pkgs.fd
+      ];
+      text = ''
+        # shellcheck disable=SC2016
+        # This script is used to find all the top level git folders in a directory
+        _get_git_folders() {
+          local dir="$1"
+          local git_folder
+
+          # throw error if no directory is passed
+          if [ -z "$dir" ]; then
+            echo "No directory passed"
+            exit 1
+          fi
+
+          while read -r git_folder; do
+            echo "$git_folder"
+          done < <(fd ".git$" "$dir" --type dir --hidden | xargs dirname | xargs realpath | sort)
+
+          while read -r wt_folder; do
+            echo "$wt_folder"
+          done < <(fd ".bare$" "$dir" --type dir --hidden |\
+                    xargs -I {} sh -c 'find "$(dirname {})" -maxdepth 1 -type d ! -name ".*" ! -path "$(dirname {})"' |\
+                    xargs realpath | sort)
+        }
+
+        _get_git_folders "$@"
+      '';
+    };
+  in {
+    catppuccin.eza.enable = true;
+    home.packages =
+      (with pkgs; [
+        age # A simple, modern and secure encryption tool with small explicit keys, no config options, and UNIX-style composability
+        antigen # zsh plugin manager
+        babashka # A Clojure babushka for the grey areas of bash
+        bind # dns client
+        bitwarden-cli # a secure and free password manager for all of your devices
+        bluetuith # bluetooth manager tui
+        bottom # A cross-platform graphical process/system monitor with a customizable interface and a multitude of features
+        claude-code # A command line interface for Claude AI
+        codex # A lightweight coding agent that runs in your terminal
+        curl # transfer data from or to a server
+        # crush # The glamourous AI coding agent for your favourite terminal # broken tests
+        delta # A syntax-highlighter for git and diff output
+        doggo # Command-line DNS Client for Humans.
+        dust # A more intuitive version of du in rust
+        eza # A modern replacement for ls
+        entr # Run arbitrary commands when files change
+        fd # A simple, fast and user-friendly alternative to find
+        fzf # A command-line fuzzy finder
+        framework-tool # Swiss army knife for Framework laptops
+        gh # GitHub CLI
+        glow # Render markdown on the CLI, with pizzazz!
+        handlr # Alternative to xdg-open to manage default applications with ease
+        htop # An interactive process viewer
+        instaloader # Download public and private instagram accounts
+        iputils # Network monitoring tools including ping
+        just # A command runner with a makefile-like syntax
+        jq # Command-line JSON processor
+        lazydocker # simple terminal ui for both docker and docker-compose
+        lsof # lists open files
+        mermaid-cli # Generation of diagrams and flowcharts from text in a similar manner as markdown
+        ngrok # Introspected tunnels to localhost
+        p7zip # 7-Zip is a file archiver with a high compression ratio
+        playerctl # pause/play music players ci
+        podman # A program for managing pods, containers and container images
+        podman-tui # Podman Terminal UI
+        presenterm # A terminal presentation tool
+        procs # A modern replacement for ps written in Rust
+        ripgrep-all # ripgrep but also search in Pdfs, zip, and more
+        ripgrep # recursively searches directories for a regex pattern
+        rsync # Fast incremental file transfer utility
+        rustscan # The Modern Port Scanner
+        shfmt # A shell parser, formatter, and interpreter (POSIX/Bash/mksh)
+        silver-searcher # A code-searching tool similar to ack, but faster
+        smartmontools # control and monitor storage systems using S.M.A.R.T.
+        sops # Simple and flexible tool for managing secrets
+        ssh-to-age # convert ssh private keys in ed25519 format to age keys
+        taskwarrior3 # A command-line todo list manager
+        tealdeer # A very fast implementation of tldr in Rust
+        timewarrior # A command-line time tracker
+        traceroute # print the route packets trace to network host
+        udiskie # Removable disk automounter using udisks
+        udisks # access and manipulate disks and media devices
+        vimv # batch rename files w/ vim
+        wakatime-cli # Command line interface for Wakatime
+        wget # The non-interactive network downloader # required by jdownloader
+        wiki-tui # A terminal based wikipedia viewer
+        wipe # Secure file wiping utility
+        wordnet # Lexical database for the English language
+        xdotool # Command-line X11 automation tool
+        dragon-drop # simple drag and drop source/sink for x11
+        xh # Friendly and fast tool for sending HTTP requests,  curl/wget
+        yay # yet another aur helper
+        yadm # Yet Another Dotfiles Manager
+        yt-dlp # A youtube-dl fork with additional features and fixes
+        yubikey-manager # A command line tool to manage YubiKeys
+        zellij # A terminal workspace and multiplexer
+        zoxide # A fast alternative to cd that learns your habits
+        zsh # A shell designed for interactive use, although it is also a powerful scripting language
+      ])
+      ++ [
+        getOptions # A library for parsing command line options
+        projects # A script to find all the top level git folders in a directory
+      ];
+
+    xdg.configFile."taskell/config.ini".text = lib.generators.toINI {} {
+      general = {
+        filename = "kanban.md";
+      };
+
+      layout = {
+        padding = 1;
+        column_width = 30;
+        column_padding = 3;
+        description_indicator = "≡";
+        statusbar = true;
+      };
+
+      markdown = {
+        title = "##";
+        task = "-";
+        summary = "    >";
+        due = "    @";
+        subtask = "    *";
+        localTimes = false;
+      };
+    };
+
+    xdg.configFile."zsh/nix-packages.zsh".source = pkgs.writeText "nix-packages" ''
+      source "${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
+    '';
+
+    xdg.configFile."containers/registries.conf".text = ''
+      unqualified-search-registries = ["docker.io"]
+
+      [[registry]]
+      prefix = "docker.io"
+      insecure = false
+      blocked = false
+      location = "docker.io"
+    '';
+
+    catppuccin.bat.enable = true;
+    catppuccin.lazygit.enable = true;
+    programs = {
+      # A cat(1) clone with wings
+      bat = {
+        enable = true;
+
+        config.map-syntax = [
+          "*.fnl:Clojure"
+          "*.templ:Go"
+          "flake.lock:JSON"
+        ];
+
+        extraPackages = with pkgs.bat-extras; [
+          batman
+          batpipe
+        ];
+      };
+
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+
+      atuin = {
+        enable = true;
+        settings = {
+          enter_accept = false;
+          keymap_mode = "vim-normal";
+          inline_height = 20;
+        };
+      };
+
+      # Blazingly fast terminal file manager
+      yazi = {
+        enable = true;
+        settings = {
+          mgr = {
+            show_hidden = true;
+          };
+          opener.open = [
+            {
+              run = ''xdg-open "$1"'';
+              desc = "Open";
+              orphan = true;
+            }
+          ];
+        };
+      };
+
+      lazygit = {
+        enable = true;
+        settings = {
+          customCommands = [
+            {
+              key = "w";
+              context = "files";
+              command = "git commit --no-verify";
+              description = "Commit without verifying";
+              output = "terminal";
+            }
+          ];
+          gui.theme = {
+            selectedLineBgColor = ["default"];
+          };
+        };
+      };
+    };
+  };
+}
